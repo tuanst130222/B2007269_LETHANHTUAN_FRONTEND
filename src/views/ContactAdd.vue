233122a28493eadb87b9432dd9e74dce1b3b48d1
Rob Pear <tuanst130222@gmail.com>
2023-11-02 09:55:47 +0700

    Hien thi danh sach cac contact lay ve tu server

---
+++ src/views/ContactAdd.vue
@@ -0,0 +1,33 @@
+<template>
+     <div v-if="status" class="page">
+          <h4>Thêm mới Liên hệ</h4>
+          <ContactForm :contact="contact" @submit:contact="createContact" />
+          <p>{{ message }}</p>
+     </div>
+</template>
+<script>
+import ContactForm from "@/components/ContactForm.vue";
+import ContactService from "@/services/contact.service";
+export default {
+     components: {
+          ContactForm,
+     },
+     data() {
+          return {
+               contact: {},
+               message: "",
+               status: true
+          };
+     },
+     methods: {
+          async createContact(data) {
+               try {
+                    await ContactService.create(data);
+                    this.message = "Liên hệ được thêm mới thành công.";
+               } catch (error) {
+                    console.log(error);
+               }
+          },
+     },
+};
+</script>
